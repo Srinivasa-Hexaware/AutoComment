c0b5c450fa8a0edbb7551c7b51064210797cae5f
Srinivasa-Hexaware <136800591+Srinivasa-Hexaware@users.noreply.github.com>
2024-08-06 15:34:55 +0530

    Update config.py
    Changed the secret creds.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-endpoint = "https://hv-openai-lab66.openai.azure.com/"
-apikey = "6298b831d51c4efca8a2cf2412b8b70e"
-api_version="2024-05-01-preview"
-deployment_name="AutoComment"
+endpoint = "Your azure endpoint url"
+apikey = "Your azure apikey"
+api_version="Api version of service"
+deployment_name="Model deployed name"
 
 role = "user"
